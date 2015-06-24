9d667942aec6428491814db2364b067006188c9f
Mike Hopper <drmikeh@gmail.com>
2015-06-24 14:53:47 -0400

    Implemented the Items Controller and Items Filter.

--- client/app/items/items.controller.js
+++ client/app/items/items.controller.js
@@ -1,6 +1,34 @@
 'use strict';
 
 angular.module('gaCampingStoreApp')
-  .controller('ItemsCtrl', function() {
-    this.message = 'Hello';
-  });
+.controller('ItemsCtrl', function($state, itemService, cartService) {
+
+  this.searchText = '';
+  this.inventory = itemService.inventory;
+  this.cart = cartService.cart;
+
+  this.addItem = function(item) {
+    cartService.addItem(item);
+  };
+
+  this.removeItem = function(item) {
+    cartService.removeItem(item);
+  };
+
+  this.getCost = function(item) {
+    return cartService.getCost(item);
+  };
+
+  this.getTotal = function() {
+    return cartService.getTotal();
+  };
+
+  this.clearCart = function() {
+    return cartService.clearCart();
+  };
+
+  this.goItem = function (item) {
+    console.log('goItem: ' + item._id);
+    $state.go( 'itemDetail', { itemId : item._id } );
+  };
+});
