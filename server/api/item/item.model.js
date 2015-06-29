9a5e9c3fcb465c96db5a28748cd8fed25c64d7f3
Mike Hopper <drmikeh@gmail.com>
2015-06-29 09:48:45 -0400

    Minor change to item.model.js

--- server/api/item/item.model.js
+++ server/api/item/item.model.js
@@ -8,7 +8,7 @@ var ItemSchema = new Schema({
   category:    String,
   price:       { type: Number, min: 0, max: 9999.99 },
   qty:         { type: Number, min: 0, max: 999 },
-  rating:      { type: Number, min: 0, max: 5.0 },
+  rating:      { type: Number, "default": 0, min: 0, max: 5.0 },
   description: String,
   imageFile:   String
 });
